6181afafb401e9f30a742ab314579c98085ba4fa
TonysChaos <tigahteeth@icloud.com>
2026-08-01 15:33:29 -0400

    Change Supabase publishable key
    Updated the Supabase publishable key in the configuration file.

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://ydmyvrueejaeuuefwffi.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_WU2fcMzrNLO_s0K7G0lmdQ_lFo5rJbc";
 
 export const supabase = createClient(
   SUPABASE_URL,
